23df9d473d7c18235b1fd402bf2f539c87586ada
nkt.brem <nkt.brem@gmail.com>
2022-08-21 20:01:04 +0300

    complete 1.0.0

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,10 @@
-//MOBILE MENU
+//------------------------------LANGUAGES-----------------------------//
+const langBtn = document.getElementById('languages-btn');
+langBtn.addEventListener('click', () => {
+    langBtn.nextElementSibling.classList.toggle('languages__list-show')
+})
+
+//------------------------------MOBILE MENU-----------------------------//
 
 const mobileMenu = document.getElementById('mobile-menu');
 const burgerOpen = document.getElementById('burger-open');
@@ -14,8 +20,20 @@ burgerClose.addEventListener('click', () => {
     body.classList.remove('modal-open');
 })
 
+//------------------------------PLAY VIDEO-----------------------------//
 
-// FAQ ACCORDIONS
+const videoPlayBtn = document.getElementById('video-play');
+const videoThumbnail = document.getElementById('video-thumbnail');
+const videoPlayer = document.getElementById('video-iframe');
+videoPlayBtn.addEventListener('click', () => {
+    videoThumbnail.classList.add('video-active');
+    videoPlayBtn.classList.add('video-active');
+    let videoSrc = videoPlayer.getAttribute('src') + '?autoplay=1';
+    videoPlayer.setAttribute('src', videoSrc);
+})
+
+
+//------------------------------FAQ ACCORDIONS-----------------------------//
 
 const accordionTitles = document.querySelectorAll('.accordion__title');
 
@@ -23,7 +41,6 @@ accordionTitles.forEach(accordionTitle => {
     accordionTitle.addEventListener('click', event => {
         accordionTitle.classList.toggle('accordion__title-active');
         const accordionBody = accordionTitle.nextElementSibling;
-        console.log(accordionBody);
         if (accordionTitle.classList.contains('accordion__title-active')) {
             accordionBody.style.maxHeight = accordionBody.scrollHeight + 'px';
         } else {
@@ -33,145 +50,130 @@ accordionTitles.forEach(accordionTitle => {
 })
 
 
-//MODAL
-
+//------------------------------MODAL-----------------------------//
+const openModal = document.querySelectorAll('[data-contact]');
 const body = document.querySelector('body')
 const modal = document.getElementById('modal');
-const headerBtn = document.getElementById('header-btn');
-const storesBtn = document.getElementById('stores-btn');
 const close = document.getElementById('modal-close');
 
 const modalForm = document.querySelector('.modal__form')
 const modalComplete = document.querySelector('.modal__complete')
 const submit = document.getElementById('modal-submit')
 
+openModal.forEach(btn => {
+    btn.addEventListener('click', () => {
+        mobileMenu.classList.remove('mobile-show');
+        modal.classList.add('modal-show');
+        body.classList.add('modal-open')
+    })
+})
 
-headerBtn.onclick = function() {
-    modal.classList.add('modal-show');
-    body.classList.add('modal-open')
-
-}
-storesBtn.onclick = function() {
-    modal.classList.add('modal-show');
-    body.classList.add('modal-open');
-}
-
-close.onclick = function() {
+close.addEventListener('click', () => {
     modal.classList.remove('modal-show');
     body.classList.remove('modal-open');
 
     //RESET FORM
     modalForm.classList.remove('modal__form-hidden')
     modalComplete.classList.add('complete-hidden')
-}
+})
 
-document.onclick = function(event) {
+//Close when click outside of modal
+document.addEventListener('click', (event) => {
     if (event.target === modal) {
         modal.classList.remove('modal-show');
         body.classList.remove('modal-open')
+        modalForm.classList.remove('modal__form-hidden')
+        modalComplete.classList.add('complete-hidden')
     }
-}
+})
+
 
 // FORM VALIDATION NEEDED
+submit.addEventListener('click', () => {
+    modalForm.classList.add('modal__form-hidden')
+    modalComplete.classList.remove('complete-hidden')
+})
+
 
 submit.onclick = function () {
     modalForm.classList.add('modal__form-hidden')
     modalComplete.classList.remove('complete-hidden')
 }
 
-//STORES
+//------------------------------STORES CARDS-----------------------------//
 
 let storesArr = [
     {
         'logoUrl': '../../img/stores/breezy.png',
-        'description' : 'Short description of this item that focuses on the benefits or success it brings your customer.',
+        'description': 'Short description of this item that focuses on the benefits or success it brings your customer.',
         'url': 'https://www.breezy.by',
         'country': 'ukr'
     },
     {
         'logoUrl': '../../img/stores/i-store.png',
-        'description' : 'Short description of this item that focuses on the benefits or success it brings your customer.',
+        'description': 'Short description of this item that focuses on the benefits or success it brings your customer.',
         'url': 'https://www.i-store.by',
         'country': 'ukr'
     },
     {
         'logoUrl': '../../img/stores/21vek.png',
-        'description' : 'Short description of this item that focuses on the benefits or success it brings your customer.',
+        'description': 'Short description of this item that focuses on the benefits or success it brings your customer.',
         'url': 'https://www.21vek.by',
         'country': 'ukr'
     },
     {
         'logoUrl': '../../img/stores/5el.png',
-        'description' : 'Short description of this item that focuses on the benefits or success it brings your customer.',
+        'description': 'Short description of this item that focuses on the benefits or success it brings your customer.',
         'url': 'https://www.5element.by',
         'country': 'ukr'
     }
 ]
 
 const storesBlock = document.getElementById('stores');
-const allBtn = document.getElementById('all');
-const ukrBtn = document.getElementById('ukr');
-const blrBtn = document.getElementById('blr');
-const kzBtn = document.getElementById('kz');
-const cprBtn = document.getElementById('cpr');
-//OR ADD DATA ATTRIBUTES TO BUTTONS
+const countryBtns = document.querySelectorAll('[data-country]');
 
-allBtn.addEventListener('click', () => {
-    insertCards(storesArr);
+countryBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        countryBtns.forEach(btn => {
+            btn.classList.remove('locations__item-active');
+        })
+        btn.classList.add('locations__item-active');
+        storesBlock.innerHTML = '';
+        insertCards(btn.dataset.country)
+    })
 })
 
-// function sortCards(arr, country) {
-//     let sortedArr = arr.forEach(store => {
-//         store.filter
-//     })
-// }
-//
-// function insertCards(country) {
-//     if(country.length === 0) {
-//         storesBlock.map((...) => {....})
-//     } else {
-//         const selectedCountry = storesBlock.find(store => store.country === country);
-//
-//         selectedCountry.map((...) => {...})
-//     }
-// }
-//
-// arr.map((store) => {
-//     storesBlock.innerHTML += `
-//   <li class="card">
-//     <div class="card__logo">
-//       <img src="${store.image}" alt="Store Image"/>
-//     </div>
-//     <p class="card__description">${store.description}</p>
-//     <a href="${store.link}" target="_blank">${store.link}</a>
-//   </li>
-//   `;
-// });
 
-// storesBlock.innerHTML = ''
+function insertCards(country) {
+    if (country === 'all') {
+        storesArr.map((store) => {
+            storesBlock.innerHTML += `
+  <li class="card">
+    <div class="card__logo">
+      <img class="card__img" src="${store.logoUrl}" alt="Store Image"/>
+    </div>
+    <p class="card__description">${store.description}</p>
+    <a class="card__url" href="${store.url}" target="_blank">${store.url}</a>
+  </li>
+  `;
+        });
+    } else {
+        const selectedCountry = storesArr.filter(store => store.country === country);
+        selectedCountry.map((store) => {
+            storesBlock.innerHTML += `
+  <li class="card">
+    <div class="card__logo">
+      <img class="card__img" src="${store.logoUrl}" alt="Store Image"/>
+    </div>
+    <p class="card__description">${store.description}</p>
+    <a class="card__url" href="${store.url}" target="_blank">${store.url}</a>
+  </li>
+  `;
+        });
+    }
+}
 
-
-storesArr.forEach(store => {
-        let card = document.createElement('li');
-        card.classList.add('card');
-        let logo = document.createElement('div');
-        logo.classList.add('card__logo');
-        let logoImg = document.createElement('img');
-        logoImg.setAttribute('src', store.logoUrl)
-        let text = document.createElement('p');
-        text.classList.add('card__description');
-        text.textContent = store.description;
-        let url = document.createElement('a');
-        url.classList.add('card__url');
-        url.textContent = store.url;
-        url.setAttribute('href', store.url);
-        url.setAttribute('target', '_blank');
-        url.setAttribute('rel', 'noopener noreferrer');
-
-        logo.append(logoImg);
-        card.append(logo, text, url);
-        storesBlock.append(card)
-    })
+insertCards('all');
 
 
 
